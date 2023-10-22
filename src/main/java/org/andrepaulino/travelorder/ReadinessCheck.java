15c2125a90ac90c6b78fed9e03bb017a5abde039
F0738353 Andre Luiz Paulino De Sousa <andrepaulinobtw@gmail.com>
2023-10-22 04:32:53 -0300

    feat: add Liveness & Readiness check

---
+++ src/main/java/org/andrepaulino/travelorder/ReadinessCheck.java
@@ -0,0 +1,28 @@
+package org.andrepaulino.travelorder;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import jakarta.inject.Inject;
+
+@Readiness
+public class ReadinessCheck implements HealthCheck {
+
+    @Inject
+    @RestClient
+    FlightService flightService;
+
+    @Inject
+    @RestClient
+    HotelService hotelService;
+
+    @Override
+    public HealthCheckResponse call() {
+        if ((flightService.findById(1) != null) && (hotelService.findById(1) != null))
+            return HealthCheckResponse.up("Estou pronto!");
+        else
+            return HealthCheckResponse.down("Não estou pronto!");
+    }
+}
