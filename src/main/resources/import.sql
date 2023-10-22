2f4eb9f22c50dff5284d666be041111fdf4d4d30
F0738353 Andre Luiz Paulino De Sousa <andrepaulinobtw@gmail.com>
2023-10-22 10:57:13 -0300

    chore: update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-create sequence travelorder_sequence start with 1 increment by 1;
-INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
+-- create sequence travelorder_sequence start with 1 increment by 1;
+-- INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
+-- INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
+-- INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
+-- INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
+-- INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
